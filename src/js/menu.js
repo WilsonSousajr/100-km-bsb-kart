ea1fc9db90c439740d7b89ba6c48f03ab6111fac
Wilson Júnior <willnapolitano21@gmail.com>
2022-01-25 22:36:15 -0300

    Create a menu.js and make a hamburguer_menu object, and add a addEventListener to the hamburguer menu

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+const hamburguer =  document.querySelector("#hamburguer")
+
+const hamburguer_menu = {
+    top: document.querySelector("#top"),
+    middle: document.querySelector("#middle"),
+    bottom: document.querySelector("#bottom")
+}
+
+hamburguer.addEventListener("click", function(){
+    hamburguer_menu.top.classList.toggle('hamburguer-top-rotate')
+    hamburguer_menu.middle.classList.toggle('hidden')
+    hamburguer_menu.bottom.classList.toggle('hamburguer-bottom-rotate')
+})
