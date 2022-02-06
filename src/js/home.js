811e799dde09fea9209061e304143e0d2bb9f248
Wilson Júnior <willnapolitano21@gmail.com>
2022-02-05 21:58:45 -0300

    Create makeCards function

--- src/js/home.js
+++ src/js/home.js
@@ -3,6 +3,22 @@ let forms_objectsJSON = localStorage.getItem('rivals')
 let forms_objects = JSON.parse(forms_objectsJSON)
 
 
-let makeCards = () => {
-    console.log("teste")
+
+let makeCards = (forms) => {
+    form_card_group.innerHTML += `
+    
+        <div id="form-card" class="form-card">
+
+            <h3 class="categorie">${forms.category}</h3>
+            <div class="name-age-avatar">
+                <h3>${forms.first_name} ${forms.last_name}</h3>
+                <img src="./src/imgs/user.png"/>
+                <h3>${forms.age}</h3>
+            </div>
+            <div class="email-number">
+                <h3 class="email">${forms.email}</h3>
+                <h3 class="phone-number">(${forms.ddd}) ${forms.phone_number}</h3>
+            </div>
+        </div>
+    `
 }
